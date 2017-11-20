1fc3d622105c50e20f61d0d683d8c27d096b6914
Will Vesey <willvesey2@gmail.com>
2017-11-20 10:16:23 -0500

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,23 +2,19 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+# Make sure we are building against the most recent version of the site
+git submodule update --init --recursive
+
 # Build the project.
-hugo -t hugo-nuo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -t hugo-nuo
 
-# Go To Public folder
+# Commit and push changes to website submodule
 cd public
-# Add changes to git.
 git add .
-
-# Commit changes.
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
-
-# Push source and build repos.
 git push origin master
-
-# Come Back up to the Project Root
 cd ..
