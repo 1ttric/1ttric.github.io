157e618f2cea31cb238a647ab20b7002725a55b5
Will Vesey <will@vesey.tech>
2021-11-29 18:55:25 -0500

    use jetbrains mono via google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
     darkMode: false,
     theme: {
         fontFamily: {
-            "mono": "JetBrainsMonoNL"
+            "mono": "JetBrains Mono"
         },
         // Add base colors to Tailwind CSS
         colors: {
