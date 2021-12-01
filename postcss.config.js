c5bc738737fbd691a5717560bbfe9a4369de6901
Will Vesey <will@vesey.tech>
2021-11-30 20:31:25 -0500

    inline google fonts css at postcss compile time with postcss-import-url

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {}
-    }
+    plugins: [
+        require("postcss-import-url")({modernBrowser: true}),
+        require("tailwindcss"),
+        require("autoprefixer"),
+    ]
 }
