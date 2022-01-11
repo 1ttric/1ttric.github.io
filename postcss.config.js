b3455d73dc07dd0d84bd3b0b429c5c7ba4b521a1
Will Vesey <will@vesey.tech>
2022-01-11 18:15:28 -0500

    add additional flex values to tailwind config

--- postcss.config.js
+++ postcss.config.js
@@ -15,6 +15,16 @@ module.exports = {
                     "mono": ["JetBrains Mono", "mono"],
                 },
                 extend: {
+                    flex: {
+                        "2": "2 2 0%",
+                        "3": "3 3 0%",
+                        "4": "4 4 0%",
+                        "5": "5 5 0%",
+                        "6": "6 6 0%",
+                        "7": "7 7 0%",
+                        "8": "8 8 0%",
+                        "9": "9 9 0%",
+                    },
                     // Add custom colors to Tailwind CSS
                     colors: {
                         "bg0": {DEFAULT: "#000000"},
